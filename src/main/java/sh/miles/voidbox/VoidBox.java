13b7a5eed86ab8646f9f60a0d06647ee33289842
Miles Holder <mwholder2005@gmail.com>
2022-12-08 16:29:23 -0600

    first commit

---
+++ src/main/java/sh/miles/voidbox/VoidBox.java
@@ -0,0 +1,12 @@
+package sh.miles.voidbox;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class VoidBox extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("VoidBox has been enabled!");
+    }
+
+}
